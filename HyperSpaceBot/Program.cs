5c9f0dc5f723ebd641770ff8573ed443172cb6fc
Hugo Silva <a76954@uminho.pt>
2018-01-21 20:50:03 +0000

    2nd commit

--- HyperSpaceBot/Program.cs
+++ HyperSpaceBot/Program.cs
@@ -5,13 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
+using System.IO;
 
 namespace HyperSpaceBot
 {
     class Program
     {
         private DiscordSocketClient _client;
-        private string _token = "NDA0MzI4NTUxMjg3MDk1MzA2.DUUhvA.5LTNTWUoaicDAtMEHul-_mbW5kA";
+        private string _token = "";
         private CommandHandler _handler;
         static void Main(string[] args)
         => new Program().StartAsync().GetAwaiter().GetResult();
@@ -20,6 +21,18 @@ namespace HyperSpaceBot
         {
             _client = new DiscordSocketClient();
             _handler = new CommandHandler(_client);
+
+            try
+            {
+                StreamReader sr = new StreamReader("config.txt");
+                _token = sr.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+       
             await _client.LoginAsync(TokenType.Bot, _token);
             await _client.StartAsync();
             await Task.Delay(-1);
